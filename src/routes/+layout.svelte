04bb993f1bf96f48af8eb9788bfcfb4e5224bbd8
NathanGrace <nathan.cole.grace@gmail.com>
2024-11-13 00:01:59 -0600

    daisy works

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,25 @@
 	let { children } = $props();
 </script>
 
+
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a href="./" class="btn btn-ghost text-xl">daisyUI</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="./">Link</a></li>
+			<li>
+				<details>
+					<summary>Parent</summary>
+					<ul class="bg-base-100 rounded-t-none p-2">
+						<li><a href="./">Link 1</a></li>
+						<li><a href="./">Link 2</a></li>
+					</ul>
+				</details>
+			</li>
+		</ul>
+	</div>
+</div>
+
 {@render children()}
